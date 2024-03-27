3d197c6d5083c721080b320b3ffd2444db3a0770
mbraniff <mbraniff@alarm.com>
2024-03-27 12:52:15 -0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Matthew Braniff on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
